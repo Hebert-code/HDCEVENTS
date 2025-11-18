921cc92064af5b0762a27a78cde83c73ccdf1aab
Hebert Henrique <hebert.hb.brito@gmail.com>
2025-11-18 10:23:40 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function (){
+    return view('contact');
+});
+
+Route::get('produtos', function(){
+    return view('products');
+});
+
+Route::get('produtos/{id?}', function($id){
+    return view('products', ['id' => $id]);
+});
