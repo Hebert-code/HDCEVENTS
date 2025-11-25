a2f365bd0d837f04e9a526deb22fc78144cf8f00
Hebert Henrique <hebert.hb.brito@gmail.com>
2025-11-25 13:53:18 -0300

    Feat: Update

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,10 +8,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Event extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'date',
+        'city',
+        'private',
+        'description',
+        'image',
+        'items',
+        'user_id',
+    ];
 
     protected $casts = [
         'items' => 'array',
+        'date'  => 'datetime',
     ];
 
     protected $dates = ['date'];
+
+    public function user(){
+        return $this->belongsTo('App\Models\User');
+    }
 }
